2c184b0ee63b74e6926727d644b36cb072dd64a0
Artem Arafailov <artem.ar888@gmail.com>
2025-10-12 15:12:53 +0300

    The third task is completely ready

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,19 @@
+"use strict"
+
 function compareArrays(arr1, arr2) {
-  
+    if (!(Array.isArray(arr1) && Array.isArray(arr2) && (arr1.length === arr2.length))) {
+        return false;
+    }
+    return arr1.every((item, index) => item === arr2[index]);  
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+    let result = users.filter(user => user.gender === gender).reduce((acc, user, index, arr) => {
+        acc += user.age;
+        if (index === arr.length -1) {
+            return acc / arr.length;
+        }
+        return acc;
+    }, 0);
+    return result;
 }
